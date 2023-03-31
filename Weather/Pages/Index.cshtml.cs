f6246cd873dfa1c72cb20b7383d84ac7c1ff66a9
Serjey <ls.xcrone@gmail.com>
2023-03-31 23:57:38 +0300

    add: options config

--- Weather/Pages/Index.cshtml.cs
+++ Weather/Pages/Index.cshtml.cs
@@ -1,20 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
+using Weather.Models;
 
 namespace Weather.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly ApiKeys _apiKeys;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IOptions<ApiKeys> _options;
+
+        public IndexModel(ILogger<IndexModel> logger, IOptions<ApiKeys> options)
         {
             _logger = logger;
+            _options = options;
+            _apiKeys = options.Value;
         }
 
         public void OnGet()
         {
-
         }
     }
 }
