b80dc4eb4af15c11bb4004910c44ac867ea484c5
Serjey <ls.xcrone@gmail.com>
2023-04-01 00:00:37 +0300

    join: methods

--- Weather.Services/Interfaces/IForecastService.cs
+++ Weather.Services/Interfaces/IForecastService.cs
@@ -1,9 +1,16 @@
 ﻿using Weather.Domain.Entities;
+using Weather.Domain.Interfaces;
 using Weather.Domain.ViewModels;
 
 namespace Weather.Services.Interfaces
 {
     public interface IForecastService : IBaseService<Forecast, ForecastViewModel>
     {
+        Task<IDefaultResponse<Forecast>> GetItemByCityName(SearchForecastByNameViewModel model);
+
+        Task<IDefaultResponse<Forecast>> GetItemByCityCoord(SearchForecastByGeoViewModel model);
+
+        Task<IDefaultResponse<Forecast>> GetItemByCityId(int cityId);
+
     }
 }
