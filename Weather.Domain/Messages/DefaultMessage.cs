be7ff34b07154bc4297bc280593c8e948dc18df9
Serjey <ls.xcrone@gmail.com>
2023-04-03 13:45:17 +0300

    ref: add readonly

--- Weather.Domain/Messages/DefaultMessage.cs
+++ Weather.Domain/Messages/DefaultMessage.cs
@@ -2,30 +2,30 @@
 {
     public static class DefaultMessage
     {
-        public static string NotFound = "Not found";
+        public static readonly string NotFound = "Not found";
 
-        public static string CreateSucces = "Create succes";
+        public static readonly string CreateSucces = "Create succes";
 
-        public static string RestoreSucces = "Restore succes";
+        public static readonly string RestoreSucces = "Restore succes";
 
-        public static string RemoveSucces = "Remove succes";
+        public static readonly string RemoveSucces = "Remove succes";
 
-        public static string UpdateSucces = "Update succes";
+        public static readonly string UpdateSucces = "Update succes";
 
-        public static string EmailIsBusy = "Email is busy";
+        public static readonly string EmailIsBusy = "Email is busy";
 
-        public static string NewItemNotFound = "New item not found";
+        public static readonly string NewItemNotFound = "New item not found";
 
-        public static string IncorrectEmilOrPassword = "Incorrect email or password";
+        public static readonly string IncorrectEmilOrPassword = "Incorrect email or password";
 
-        public static string UserNotFound = "User not found";
+        public static readonly string UserNotFound = "User not found";
 
-        public static string ErrorSave = "Error save";
+        public static readonly string ErrorSave = "Error save";
 
-        public static string ErrorUpdate = "Error update";
+        public static readonly string ErrorUpdate = "Error update";
 
-        public static string NoNeedToUpdate = "No need to update";
+        public static readonly string NoNeedToUpdate = "No need to update";
 
-        public static string CityNotFound = "City not found";
+        public static readonly string CityNotFound = "City not found";
     }
 }
